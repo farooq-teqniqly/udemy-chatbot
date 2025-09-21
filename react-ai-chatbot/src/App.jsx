81f454818a6976331e08dba96179bf4d5593cd9c
farooq-teqniqly <farooq@teqniqly.com>
2025-09-21 11:46:32 -0700

    chore: Display dummy chat messages.

--- react-ai-chatbot/src/App.jsx
+++ react-ai-chatbot/src/App.jsx
@@ -1,15 +1,56 @@
+import {useState} from "react";
 import styles from "./App.module.css";
+import {Chat} from "./components/Chat/Chat";
 
 function App() {
+    const [messages, _] = useState(MESSAGES);
+
     return (
         <div className={styles.App}>
             <header className={styles.Header}>
                 <img src="/chat-bot.png" alt="AI Chat Bot" className={styles.Logo}/>
                 <h2 className={styles.Title}>AI Chatbot</h2>
             </header>
-            <div className={styles.ChatContainer}></div>
+            <div className={styles.ChatContainer}>
+                <Chat messages={messages}></Chat>
+            </div>
         </div>
     )
 }
+
+const MESSAGES = [
+    {
+        role: "user",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "assistant",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "user",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "assistant",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "user",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "assistant",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "user",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    },
+    {
+        role: "assistant",
+        content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
+    }
+]
 
 export default App
---
+++ react-ai-chatbot/src/components/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+import styles from "./Chat.module.css"
+
+export function Chat({messages}) {
+    return <div className={styles.Chat}>
+        {messages.map(({role, content}, index) => (
+            <div key={index} data-role={role} className={styles.Message}>{content}</div>
+        ))}
+    </div>
+}

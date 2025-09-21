255ee41b2649f286f6a5043a12c91a312623aacc
farooq-teqniqly <farooq@teqniqly.com>
2025-09-21 11:32:15 -0700

    chore: Styled the chatbot UI.

--- react-ai-chatbot/src/App.jsx
+++ react-ai-chatbot/src/App.jsx
@@ -1,6 +1,14 @@
+import styles from "./App.module.css";
+
 function App() {
     return (
-        <div>AI Chatbot</div>
+        <div className={styles.App}>
+            <header className={styles.Header}>
+                <img src="/chat-bot.png" alt="AI Chat Bot" className={styles.Logo}/>
+                <h2 className={styles.Title}>AI Chatbot</h2>
+            </header>
+            <div className={styles.ChatContainer}></div>
+        </div>
     )
 }
 
